32cb131bd5ae6885c12b7100951ea0d65e7ffa23
Ewan <ewanbeattie1@gmail.com>
2025-06-25 13:25:21 +0100

    GPU

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from models import Transformer
 from trainer import Trainer
 from config import config
 import torch
+from math import sqrt
 
 
 class Controller():
@@ -11,7 +12,13 @@ class Controller():
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         train_loader, test_loader = data.get_mnist_data(batch_size=config['batch_size'])
-        self.model = Transformer(num_patches=config['num_patches'], patch_dim=7, embedding_size=config['embedding_size'], num_layers=config['num_layers']).to(self.device)
+        patch_dim = int(28 / sqrt(config['num_patches']))
+        self.model = Transformer(num_patches=config['num_patches'], 
+                                 patch_dim=patch_dim, 
+                                 embedding_size=config['embedding_size'], 
+                                 num_layers=config['num_layers'])
+        
+        self.model.to(self.device)
         self.train_loader = train_loader
         self.test_loader = test_loader
 
@@ -31,7 +38,7 @@ class Controller():
             for data, target in self.test_loader:
                 data =  data.to(self.device)
                 target = target.to(self.device)
-                
+
                 output = self.model(data)
                 _, predicted = output.max(1)
                 correct += (predicted == target).sum().item()
