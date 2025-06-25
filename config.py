d0f42ddc0836a2ba41f39a994ee3c73c5f568df8
Ewan <ewanbeattie1@gmail.com>
2025-06-25 13:15:41 +0100

    GPU

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ config = {
     'batch_size': 64,
     'learning_rate': 0.001,
     'epochs': 1,
-    'embedding_size': 25,
+    'embedding_size': 64,
     'num_layers': 1,
     'dropout': 0.1,
-    'num_patches': 16,
+    'num_patches': 4,
 }
